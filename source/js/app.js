712124b3fc2750c38fa38e6163563f4dfc17c426
Ivan <ivanprivalov@mail.ru>
2021-01-20 02:49:33 +0300

    Making the menu expandable

---
+++ source/js/app.js
@@ -0,0 +1,19 @@
+const pageHeaderElement = document.querySelector('.page-header');
+const mainNavigationElement = document.querySelector('.main-nav');
+const navigationToggleElement = document.querySelector('.main-nav__toggle');
+
+navigationToggleElement.addEventListener('click', () => {
+  if (mainNavigationElement.classList.contains('main-nav--closed')) {
+    mainNavigationElement.classList.remove('main-nav--closed');
+    pageHeaderElement.classList.remove('page-header--closed-menu');
+
+    mainNavigationElement.classList.add('main-nav--opened');
+    pageHeaderElement.classList.add('page-header--opened-menu');
+  } else {
+    mainNavigationElement.classList.add('main-nav--closed');
+    pageHeaderElement.classList.add('page-header--closed-menu');
+
+    mainNavigationElement.classList.remove('main-nav--opened');
+    pageHeaderElement.classList.add('page-header--opened-menu');
+  }
+});
